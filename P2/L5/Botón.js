f1c160c48aa3d41f211640baf5761fba06b71b8e
ignardt <i.andres.2019@alumnos.urjc.es>
2022-03-16 17:25:42 +0100

    .

--- P2/L5/Botón.js
+++
@@ -1,9 +0,0 @@
-console.log("Pulse el botón");
-
-const test = document.getElementById('test')
-
-//-- Configurar el manejador para el evento de
-//-- pulsación de botón
-GamepadButton.onclick = function () {
-  console.log("Botón pulsado")
-}
