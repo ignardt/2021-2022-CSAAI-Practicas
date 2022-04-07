6a96ba173ba3c85a6fdc6549adacf911641a985e
ignardt <i.andres.2019@alumnos.urjc.es>
2022-04-07 04:32:13 +0200

    .

---
+++ P3/juego.js
@@ -0,0 +1,190 @@
+// LOAD BG IMAGE
+const BG_IMG = new Image();
+BG_IMG.src = "fondo.jpg";
+
+// SELECT CANVAS ELEMENT
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
+
+// ADD BORDER TO CANVAS
+
+
+// MAKE LINE THIK WHEN DRAWING TO CANVAS
+
+
+// GAME VARIABLES AND CONSTANTS
+const PADDLE_WIDTH = 100;
+const PADDLE_MARGIN_BOTTOM = 50;
+const PADDLE_HEIGHT = 20;
+const BALL_RADIUS = 8;
+let LIFE = 3; // PLAYER HAS 3 LIVES
+let leftArrow = false;
+let rightArrow = false;
+
+// CREATE THE PADDLE
+const paddle = {
+    x : cvs.width/2 - PADDLE_WIDTH/2,
+    y : cvs.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+    width : PADDLE_WIDTH,
+    height : PADDLE_HEIGHT,
+    dx :5
+}
+
+// DRAW PADDLE
+function drawPaddle(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+    
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+    ctx.lineWidth = 3;
+}
+
+// CONTROL THE PADDLE
+document.addEventListener("keydown", function(event){
+   if(event.keyCode == 37){
+       leftArrow = true;
+   }else if(event.keyCode == 39){
+       rightArrow = true;
+   }
+});
+document.addEventListener("keyup", function(event){
+   if(event.keyCode == 37){
+       leftArrow = false;
+   }else if(event.keyCode == 39){
+       rightArrow = false;
+   }
+});
+
+// MOVE PADDLE
+function movePaddle(){
+    if(rightArrow && paddle.x + paddle.width < cvs.width){
+        paddle.x += paddle.dx;
+    }else if(leftArrow && paddle.x > 0){
+        paddle.x -= paddle.dx;
+    }
+}
+
+// CREATE THE BALL
+const ball = {
+    x : cvs.width/2,
+    y : paddle.y - BALL_RADIUS,
+    radius : BALL_RADIUS,
+    speed : 4,
+    dx : 3 * (Math.random() * 2 - 1),
+    dy : -3
+}
+
+// DRAW THE BALL
+function drawBall(){
+    ctx.beginPath();
+    
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    
+    ctx.closePath();
+}
+
+// MOVE THE BALL
+function moveBall(){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+// BALL AND WALL COLLISION DETECTION
+function ballWallCollision(){
+    if(ball.x + ball.radius > cvs.width || ball.x - ball.radius < 0){
+        ball.dx = - ball.dx;
+    }
+    
+    if(ball.y - ball.radius < 0){
+        ball.dy = -ball.dy;
+    }
+    
+    if(ball.y + ball.radius > cvs.height){
+        LIFE--; // LOSE LIFE
+        resetBall();
+    }
+}
+
+// RESET THE BALL
+function resetBall(){
+    ball.x = cvs.width/2;
+    ball.y = paddle.y - BALL_RADIUS;
+    ball.dx = 3 * (Math.random() * 2 - 1);
+    ball.dy = -3;
+}
+
+// BALL AND PADDLE COLLISION
+function ballPaddleCollision(){
+    if(ball.x < paddle.x + paddle.width && ball.x > paddle.x && ball.y > paddle.y){
+        
+        // CHECK WHERE THE BALL HIT THE PADDLE
+        let collidePoint = ball.x - (paddle.x + paddle.width/2);
+        
+        // NORMALIZE THE VALUES
+        collidePoint = collidePoint / (paddle.width/2);
+        
+        // CALCULATE THE ANGLE OF THE BALL
+        let angle = collidePoint * Math.PI/3;
+            
+            
+        ball.dx = ball.speed * Math.sin(angle);
+        ball.dy = - ball.speed * Math.cos(angle);
+    }
+}
+
+// DRAW FUNCTION
+function draw(){
+    drawPaddle();
+    
+    drawBall();
+}
+
+// UPDATE GAME FUNCTION
+function update(){
+    movePaddle();
+    
+    moveBall();
+    
+    ballWallCollision();
+    
+    ballPaddleCollision();
+}
+
+// GAME LOOP
+function loop(){
+    // CLEAR THE CANVAS
+    ctx.drawImage(BG_IMG, 0, 0);
+    
+    draw();
+    
+    update();
+    
+    requestAnimationFrame(loop);
+}
+loop();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
